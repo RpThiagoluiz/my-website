1732791237baaa14e79c06f58520c63cb2105f45
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2023-03-17 18:50:13 -0300

    refactor

--- src/modules/Home/components/AboutMe/styles.ts
+++ src/modules/Home/components/AboutMe/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 height: 40vh;
-background: ${({ theme }) => theme.COLORS.HIGHLIGHT.CIANO};
+background: ${({ theme }) => theme.COLORS.BACKGROUND.SECONDARY};
 padding: 80px 50px 50px ;
 display: flex;
 flex-direction: column;
