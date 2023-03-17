9059f0d7b8e9abcba4557cd19da6503eeda492df
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2023-03-16 23:04:19 -0300

    fix: test

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,8 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.out/', '/public/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
   moduleNameMapper: {
-    '.*\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/mocks/fileMock.js',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/.jest/mocks/fileMock.js',
     '.*\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
   //   moduleFileExtensions: [
